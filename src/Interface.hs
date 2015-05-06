c9d222dd4e0e7d7a04dab2ea5b2bc7aa654d7afc
IliaPugach <iliapugach@gmail.com>
2015-05-06 06:12:58 +0300

    Вынес интерфейс в отдельный от main'а модуль
    Просто потому, что мне так больше нравится

--- src/Interface.hs
+++ src/Interface.hs
@@ -1,77 +1,106 @@
 module Interface where
 import Core
-import Main
+import Types
 import Graphics.Gloss.Interface.Pure.Game
 
-type World = Move
+--type World = State
 
-interface :: IO ()
+-- окно всегда 3 на 4
+-- все сделано под базовый размер окна 640 на 480,
+-- потому что так при масшатбировании оно лучше смотрится
+-- и потому, что так не нужно заново подбирать волшебные константы, 
+-- отвечающие за положение объектов интерфейса
+windowWidth = 800
+windowHeight = windowWidth*3/4
+scaleFactor = windowHeight/480
+-- константа gridNumber определяет количество клеток
+-- на данный момент скорее для красоты
+-- потому что она не поддерживается core-частью программы
+gridNumber = 8
+gNumber = ceiling(gridNumber)
+gridSize = 480/(gridNumber+2)
+realGridSize = gridSize*scaleFactor
+circleRadius = gridSize/2.2
+
 -- Основная функция, которая запускает графику
+interface :: IO ()
 interface = do
-  play display bgColor fps (initialize) drawWorld handleWorld updateWorld
+  play display bgColor fps (initGame) drawWorld handleWorld updateWorld
   where
-    windowSize   = (640, 480)
+    windowSize   = (ceiling windowWidth, ceiling windowHeight)
     windowOffset = (200, 200)
     display = InWindow "Reversi" windowSize windowOffset
     bgColor = white
     fps = 60
 
-drawWorld::World->Picture
 -- Функция, которая рисует все
-drawWorld (field,player,a,b) = pictures 
-    [ forAll 1 1 drawSquare field,
-    showGameOver player,
-    Translate 270 200 (drawChecker player),
-    Translate (-250) 200 (drawChecker White),
-    Translate (-280) 120 (Scale 0.5 0.5 (Text (show b))),
-    Translate (-250) (-60) (drawChecker Black),
-    Translate (-280) (-140) (Scale 0.5 0.5 (Text (show a))) ]
+drawWorld::State->Picture
+drawWorld s = scale scaleFactor scaleFactor
+    (pictures 
+    [ forAll (1, 1) (board s),
+    showGameOver (player s),
+    translate ( 270) ( 200) (drawChecker (player s)),
+    translate (-250) ( 200) (drawChecker White),
+    translate (-280) ( 120) (scale 0.5 0.5 (Text (show (fst(score s))))),
+    translate (-250) (-60 ) (drawChecker Black),
+    translate (-280) (-140) (scale 0.5 0.5 (Text (show (snd(score s))))) ])
 
-showGameOver::Player->Picture
 -- Если игра окончена, эта функция выводит соответствующую надпись на экран
-showGameOver Empty = Translate (-100) (-220)
-  (Scale 0.3 0.3 (Text "Game Over!"))
+showGameOver::Player->Picture
+showGameOver Empty = translate (-100) (-220)
+  (scale 0.3 0.3 (text "Game Over!"))
 showGameOver _ = Blank
-  
-forAll::Int->Int->(Player->Int->Int->Picture)->[[Player]]->Picture
--- Функция, которая разбирает доску на клеточки
-forAll 8 8 f ((t:ts):ls) = (drawSquare t 8 8)
-forAll 8 y f ((t:ts):ls) = pictures 
-  [(drawSquare t 8 y),(forAll 1 (y+1) f ls)]
-forAll x y f ((t:ts):ls) = pictures 
-  [(drawSquare t x y),(forAll (x+1) y f (ts:ls))]
 
-drawSquare::Player->Int->Int->Picture
+-- Функция, которая разбирает доску на клеточки  
+forAll::Cell->Field->Picture
+--forAll gridNumber gridNumber f ((t:ts):ls) =
+--  (drawSquare t gridNumber gridNumber)
+--forAll gridNumber y f ((t:ts):ls) = pictures 
+--  [(drawSquare t gridNumber y),(forAll 1 (y+1) f ls)]
+forAll (x, y) ((t:ts):ls)
+  | (x == gNumber) && (y == gNumber) = (drawSquare t (gNumber, gNumber))
+  | (x == gNumber) = pictures [(drawSquare t (gNumber, y)),(forAll (1, (y+1)) ls)]
+  | otherwise = pictures [(drawSquare t (x, y)),(forAll ((x+1), y) (ts:ls))]
+
 -- Функция, которая рисует одну клетку доски в нужном месте картинки
-drawSquare p x y = pictures 
-  [Translate (50*(fromIntegral x-4)) (50*(fromIntegral y-4)) (drawChecker p)]
+drawSquare::Player->Cell->Picture
+drawSquare p (x, y) = translate
+  (gridSize*(fromIntegral x-gridNumber/2))
+  (gridSize*(fromIntegral y-gridNumber/2))
+  (pictures [Color black (rectangleWire gridSize gridSize), (drawChecker p)])
 
+-- Функция, которая рисует одну шашку соответствующего цвета
 drawChecker::Player->Picture
--- Функция, которая непосредственно рисует одну клетку доски
-drawChecker White = pictures 
-  [Color black (rectangleWire 50 50), Color black (circle 25)]
-drawChecker Black = pictures 
-  [Color black (rectangleWire 50 50), Color black (circleSolid 25)]
-drawChecker _ = Color black (rectangleWire 50 50)
+drawChecker White = Color black (circle circleRadius)
+drawChecker Black = Color black (circleSolid circleRadius)
+drawChecker _ = Blank
 
-handleWorld::Event->World->World
 -- Функция, которая реагирует на нажатие мыши
-handleWorld (EventKey (MouseButton LeftButton) Down _ (x, y)) board = 
-  nextMoveGraphic board (move board (ceiling (y/50 + 3.5)) (ceiling (x/50 + 3.5)))
-handleWorld _ board = board
+-- При нажатии на поле пытается сделать ход в эту клетку поля
+handleWorld::Event->State->State
+handleWorld (EventKey (MouseButton LeftButton) Down _ (x, y)) s = 
+  nextMoveGraphic s 
+  (mov s 
+    ((ceiling (y/realGridSize + gridNumber/2 - 0.5)),
+     (ceiling (x/realGridSize + gridNumber/2 - 0.5))))
+handleWorld _ s = s
 
-nextMoveGraphic::Move->Revert->Move
 -- Функция, которая смотрит, "сделался" ли ход
+-- и если да, то делает его
+nextMoveGraphic::State->Maybe State->State
 nextMoveGraphic prev Nothing = prev
 nextMoveGraphic _ (Just next)
-  | (canMove next) = next
-  | (not (canMove next)) && (canMove (switchPlayer next)) = switchPlayer next
+  | (canMov next) = next
+  | (not (canMov next)) && (canMov (switchMove next)) = switchMove next
   | otherwise = (gameOver next)
-  
-gameOver::Move->Move
--- Функция, которая обнуляет текущего игрока как знак того, что игра окончена
-gameOver (f, p, a, b) = (f, Empty, a, b)
 
-updateWorld :: Float -> World -> World
--- Функция, которая теоретически может обновлять мир, но ничего не делает
+-- Функция, которая обнуляет текущего игрока как знак того, что игра окончена  
+gameOver::State->State
+gameOver s = State f Empty sc
+    where
+        f = (board s)
+        sc = (score s)
+
+-- Функция, которая теоретически может обновлять мир по времени, но ничего не делает
+updateWorld :: Float -> State -> State
 updateWorld _ = id
